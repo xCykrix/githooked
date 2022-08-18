eb018beab60e5b6cb90b8a4ded0e8c47b9f85b68
Samuel Voeller <samuel.voeller@amethyst.live>
2022-08-18 17:01:07 -0400

    feat: add buildscript and ignore built files

--- BUILD.sh
+++ BUILD.sh
@@ -3,7 +3,7 @@ deno compile \
   --allow-run=deno,git \
   --allow-read=.git,.git-hooks \
   --allow-write=.git-hooks \
-  --output=$PWD/build/githooked \
+  --output=$PWD/build/githooked_linux \
   --target=x86_64-unknown-linux-gnu \
   https://raw.githubusercontent.com/amethyst-studio/githooked/main/mod.ts install
 
@@ -12,16 +12,7 @@ deno compile \
   --allow-run=deno,git \
   --allow-read=.git,.git-hooks \
   --allow-write=.git-hooks \
-  --output=$PWD/build/githooked.exe \
-  --target=x86_64-pc-windows-msvc \
-  https://raw.githubusercontent.com/amethyst-studio/githooked/main/mod.ts install
-
-deno compile \
-  --no-check=remote \
-  --allow-run=deno,git \
-  --allow-read=.git,.git-hooks \
-  --allow-write=.git-hooks \
-  --output=$PWD/build/githooked_x86_apple_darwin \
+  --output=$PWD/build/githooked_x86_apple \
   --target=x86_64-apple-darwin \
   https://raw.githubusercontent.com/amethyst-studio/githooked/main/mod.ts install
 
@@ -30,6 +21,6 @@ deno compile \
   --allow-run=deno,git \
   --allow-read=.git,.git-hooks \
   --allow-write=.git-hooks \
-  --output=$PWD/build/githooked_aarch64_apple_darwin \
+  --output=$PWD/build/githooked_aarch64_apple \
   --target=aarch64-apple-darwin \
   https://raw.githubusercontent.com/amethyst-studio/githooked/main/mod.ts install
