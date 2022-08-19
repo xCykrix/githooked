4abe64905350278201ae037c6dc9ab5ab63d1232
Samuel Voeller <samuel.voeller@amethyst.live>
2022-08-19 14:44:42 -0400

    fix: update build script permissions and typo

--- BUILD.sh
+++ BUILD.sh
@@ -3,7 +3,7 @@ rm -r ./build/
 deno compile \
   --no-check=remote \
   --allow-run=deno,git \
-  --allow-read=.git,.git-hooks \
+  --allow-read \
   --allow-write=.git-hooks \
   --output=$PWD/build/githooked_linux \
   --target=x86_64-unknown-linux-gnu \
@@ -12,7 +12,7 @@ deno compile \
 deno compile \
   --no-check=remote \
   --allow-run=deno,git \
-  --allow-read=.git,.git-hooks \
+  --allow-read \
   --allow-write=.git-hooks \
   --output=$PWD/build/githooked_x86_apple \
   --target=x86_64-apple-darwin \
@@ -21,8 +21,8 @@ deno compile \
 deno compile \
   --no-check=remote \
   --allow-run=deno,git \
-  --allow-read=.git,.git-hooks \
+  --allow-read \
   --allow-write=.git-hooks \
-  --output=$PWD/build/githooked_aarch64_apple \
+  --output=$PWD/build/githooked_arch64_apple \
   --target=aarch64-apple-darwin \
   https://raw.githubusercontent.com/amethyst-studio/githooked/main/mod.ts install
